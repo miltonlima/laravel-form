4d300df3fe2dd89a5878ec08fff3748e529eb7f0
Milton Lima <milton.mlima@gmail.com>
2022-10-31 17:59:11 -0300

    teste2

---
+++ database/migrations/2022_10_13_143330_create_forms_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome');
+            $table->string('nome_artistico');
+            $table->string('cpf');
+            $table->string('data_nascimento');
+            $table->string('nacionalidade');
+            $table->string('naturalidade');
+            $table->string('email');
+            $table->string('endereco');
+            $table->string('complemento');
+            $table->string('bairro');
+            $table->string('cep');
+            $table->string('estado');
+            $table->string('cidade');
+            $table->string('telefone');
+            $table->string('vinculo_institucional');
+            $table->string('descricao_vinculo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
